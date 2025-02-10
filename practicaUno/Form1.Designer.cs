d688e0142fd50ff69b77f82ddf8286cd221935b0
hannia ponce <hanniaponce28@outlook.com>
2025-02-10 14:37:58 -0800

    Add project files.

---
+++ practicaUno/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace practicaUno
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.menuSalir = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuAcercaDe = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lstContacto = new System.Windows.Forms.ListBox();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(240, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GESTION CONTACTOS";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(234, 101);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(182, 22);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(234, 148);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(182, 22);
+            this.txtTel.TabIndex = 2;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(234, 199);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(182, 22);
+            this.txtCorreo.TabIndex = 3;
+            // 
+            // menu
+            // 
+            this.menu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuSalir,
+            this.menuAcercaDe});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(907, 28);
+            this.menu.TabIndex = 4;
+            this.menu.Text = "menuStrip1";
+            // 
+            // menuSalir
+            // 
+            this.menuSalir.Name = "menuSalir";
+            this.menuSalir.Size = new System.Drawing.Size(61, 24);
+            this.menuSalir.Text = "SALIR";
+            this.menuSalir.Click += new System.EventHandler(this.menuSalir_Click);
+            // 
+            // menuAcercaDe
+            // 
+            this.menuAcercaDe.Name = "menuAcercaDe";
+            this.menuAcercaDe.Size = new System.Drawing.Size(101, 24);
+            this.menuAcercaDe.Text = "ACERCA DE";
+            this.menuAcercaDe.Click += new System.EventHandler(this.menuAcercaDe_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "NOMBRE:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "NO. TELEFONO:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "CORREO ELECTRONICO:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(88, 308);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(106, 44);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(234, 308);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(106, 47);
+            this.btnEliminar.TabIndex = 9;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(369, 308);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(106, 47);
+            this.btnLimpiar.TabIndex = 10;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lstContacto
+            // 
+            this.lstContacto.FormattingEnabled = true;
+            this.lstContacto.ItemHeight = 16;
+            this.lstContacto.Location = new System.Drawing.Point(442, 90);
+            this.lstContacto.Name = "lstContacto";
+            this.lstContacto.Size = new System.Drawing.Size(403, 196);
+            this.lstContacto.TabIndex = 11;
+            this.lstContacto.SelectedIndexChanged += new System.EventHandler(this.lstContacto_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 450);
+            this.Controls.Add(this.lstContacto);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem menuSalir;
+        private System.Windows.Forms.ToolStripMenuItem menuAcercaDe;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.ListBox lstContacto;
+    }
+}
+
---
+++ practicaUno/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practicaUno
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            // boton para limpiar los textbox
+            txtCorreo.Clear();
+            txtNombre.Clear();
+            txtTel.Clear();
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            // se declaran las variables de tipo string
+            string nombre = txtNombre.Text;
+            string correo = txtCorreo.Text;
+            string telefono = txtTel.Text;
+
+            //se utiliza la condicion para verificar que no haya espacios en blanco
+            if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(correo) || string.IsNullOrWhiteSpace(telefono))
+            {
+                MessageBox.Show("Por favor complete todos los campos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //se agregar los datos ingresados al listbox
+            lstContacto.Items.Add($"Nombre: {nombre}, Correo: {correo}, Teléfono: {telefono}");
+
+
+        }
+
+        private void lstContacto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //se utiliza una condicion para revisar que se seleccione un contacto y asi poder eliminarlo
+            if (lstContacto.SelectedItem != null)
+            {
+                
+                lstContacto.Items.Remove(lstContacto.SelectedItem);
+            }
+            //si la condicion no se cumple se muestra un mensaje para seleccionar una opcion
+            else
+            {
+                
+                MessageBox.Show("Por favor, seleccione un ítem para eliminar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void menuSalir_Click(object sender, EventArgs e)
+        {
+            //para salir de la aplicacion
+            Application.Exit();
+        }
+
+        private void menuAcercaDe_Click(object sender, EventArgs e)
+        {
+            //se muestra mensaje con info de la aplicacion
+            MessageBox.Show("Aplicación de gestion de contactos");
+        }
+
+    }
+}
